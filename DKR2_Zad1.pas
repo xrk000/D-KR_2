b32f0fd145bfa9d8e74ad14a5f4a0cedf4e7b8ed
xrk000 <144237985+xrk000@users.noreply.github.com>
2023-11-21 02:28:49 +0300

    Add files via upload

---
+++ DKR2_Zad1.pas
@@ -0,0 +1,18 @@
+﻿program dkr2_zad1;
+
+var
+  arr: array[1..10] of integer;
+  sum, i: integer;
+
+begin
+  sum := 0;
+  for i := 1 to 10 do
+  begin
+    readln(arr[i]);
+  end;
+  for i := 2 to 10 step 2 do
+  begin
+    sum := sum + arr[i];
+  end;
+  writeln('Сумма элементов на четных позициях: ', sum);
+end.
---
+++ DKR2_Zad2.pas
@@ -0,0 +1,30 @@
+﻿program DKR2_Zad2;
+var
+  str: string;
+  w, l, i: integer;
+  iL: boolean;
+begin
+  writeln('Введите строку:');
+  readln(str);
+  w := 0;
+  l := 0;
+  iL := false;
+  for i := 1 to length(str) do
+  begin
+    if (str[i] in ['а'..'я', 'А'..'Я']) then
+    begin
+      if not iL then
+      begin
+        iL := true;
+        inc(w);
+      end;
+      inc(l);
+    end
+    else
+    begin
+      iL := false;
+    end;
+  end;
+  writeln('Число слов в строке: ', w);
+  writeln('Число букв в строке: ', l);
+end.
